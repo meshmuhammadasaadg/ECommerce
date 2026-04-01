c077c61094eb7bb857d40d1e369ac5d8dae39a8f
Muhammad Asaad <meshmuhammadasaad@gmail.com>
2026-04-01 14:43:47 +0200

    Add Features for Using Commands and Queries and Products Controller for GetAllProducts action

---
+++ ECommerce.Api/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Application.Contracts.Products;
+using ECommerce.Application.Features.Products.Queries.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpGet("")]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> GetAll()
+    {
+        var result = await _mediator.Send(new GetAllProductsQuery());
+
+        return Ok(result);
+    }
+}
---
+++ ECommerce.Application/Features/Products/Queries/Requests/GetAllProductsQuery.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ECommerce.Application.Contracts.Products;
+using ECommerce.Application.Interfaces;
+using ECommerce.Domain.Entities;
+using MediatR;
+
+namespace ECommerce.Application.Features.Products.Queries.Requests;
+
+public record GetAllProductsQuery() : IRequest<IEnumerable<ProductResponse>>;
+
+public class GetAllProductsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<GetAllProductsQuery, IEnumerable<ProductResponse>>
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IEnumerable<ProductResponse>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+    {
+        var products = await _unitOfWork.Repository<Product>().GetAllAsync();
+
+        return _mapper.Map<IEnumerable<ProductResponse>>(products);
+    }
+
+
+}
