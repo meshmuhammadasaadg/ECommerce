3165703a04321d2b5acfb0a8c59f4b05d9f5298a
Muhammad Asaad <meshmuhammadasaad@gmail.com>
2026-04-02 16:56:58 +0200

    Add Result Pattern with Error class and CategoryError

---
+++ ECommerce.Application/Abstractions/Errors/CategoryError.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Application.Abstractions.Results;
+
+namespace ECommerce.Application.Abstractions.Errors;
+
+public static class CategoryError
+{
+    public static readonly Error CategoryNotFound =
+        new("Category.NotFound", "Not Found Category with the given ID");
+}
